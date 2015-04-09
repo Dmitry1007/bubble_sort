5a87e27d2e3c17880abbcb0b5fe136a309eb86ed
Dmitry Sky <dmitry.sky@icloud.com>
2015-04-09 09:41:11 -0600

    set up file tree with test and lib folders and added Gemfile Gemfile.lock

---
+++ lib/bubble_sort.rb
@@ -0,0 +1,22 @@
+seq = [4, 3, 2, 1, 0]
+
+@swaps = 0
+
+index = 0
+
+while index + 1 < seq.length
+
+  if seq[index] > seq[index + 1]
+    seq[index], seq[index + 1] = seq[index + 1], seq[index]
+    @swaps += 1
+    index = 0
+    # print "#{seq}\n"
+  else
+    index += 1
+  end
+  result = seq
+
+end
+
+# puts "\nFinal result: #{result}"
+# puts "Swaps: #{@swaps}"
---
+++ test/bubble_sort_test.rb
@@ -0,0 +1,14 @@
+# require 'minitest'
+require 'minitest/autorun'
+require 'minitest/emoji'
+require '../lib/bubble_sort'
+
+class BubbleSortTest < Minitest::Test
+
+  # Setup the worker
+  # Do the work
+  # Check the results
+
+  # test_sort_an_empty_array
+
+end
