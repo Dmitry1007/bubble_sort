b18052b7d1bc19345fcc57a8f1b2065aab086cff
Dmitry Sky <dmitry.sky@icloud.com>
2015-04-09 10:42:37 -0600

    added potential test_cases

--- test/bubble_sort_test.rb
+++ test/bubble_sort_test.rb
@@ -11,4 +11,14 @@ class BubbleSortTest < Minitest::Test
 
   # test_sort_an_empty_array
 
+  # test_sorts_a_single_element
+
+  # test_sorts_two_in_order_elements
+
+  # test_sorts_two_elements_in_reverse_order
+
+  # test_sorts_three_elements
+
+  # test_total_victory
+
 end
